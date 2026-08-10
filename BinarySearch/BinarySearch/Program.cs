ca8482121e39668a94175004b89c20296534f9d4
Nicor <nicorbogach@gmail.com>
2026-08-10 15:32:08 +0300

    Binary Search and problem traveling salesman

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    const int MaxNumber = 100000;
+    const int Length = 10000;
+    public static void Main(string[] args)
+    {
+        int[] arr = new int[Length];
+        Random rnd = new Random();
+        for(int i = 0; i < Length; i++)
+        {
+            arr[i] = rnd.Next(1, MaxNumber);
+        }
+        Array.Sort(arr); //Necessarily
+        int findNumber = arr[rnd.Next(0, Length)], left = 0, right = Length-1, mid = right/2, resultIndex = -1;
+        while (left <= right)
+        {
+            if (arr[mid] == findNumber)
+            {
+                resultIndex = mid;
+                break;
+            }
+            else if (arr[mid] < findNumber)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+            mid = (left + right) / 2;
+        }
+        Console.WriteLine(resultIndex >= 0 ? resultIndex.ToString() : "Not found");
+    }
+}
---
+++ BinarySearch/TravelingSalesmanProblem/Program.cs
@@ -0,0 +1,58 @@
+﻿    public class Program
+    {
+        public const int Count = 5;
+        public static void Main()
+        {
+            //The problem has been solved for a small dataset, 5 place; example of the algorithm. Exact answer.
+            Random random = new Random();
+            int[,] places = new int[Count, Count];
+            for(int i = 0; i < Count; i++)
+            {
+                for (int i1 = 0; i1 < Count; i1++)
+                {
+                    places[i, i1] = random.Next(1, 10);
+                }
+            }
+
+            int[] allLength = new int[5 * 4 * 3 * 2 * 1];
+            int currentIndex = 0;       
+            for (int i = 0; i < Count; i++)
+            {
+                int lengthOne = 0, lengthTwo = 0, lengthThree = 0;
+                for (int i1 = 0; i1 < Count; i1++)
+                {
+                    if (i1 == i)
+                    {
+                        continue;
+                    }
+                    lengthOne = places[i, i1];
+                    for (int i2 = 0; i2 < Count; i2++)
+                    {
+                        if (i2 == i1 || i2 == i)
+                        {
+                            continue;
+                        }
+                        lengthTwo = lengthOne + places[i1, i2];
+                        for (int i3 = 0; i3 < Count; i3++)
+                        {
+                            if (i3 == i1 || i3 == i || i3 == i2)
+                            {
+                                continue;
+                            }
+                            lengthThree = lengthTwo + places[i2, i3];
+                            for (int i4 = 0; i4 < Count; i4++)
+                            {
+                                if (i4 == i1 || i4 == i || i4 == i2 || i4 == i3)
+                                {
+                                    continue;
+                                }
+                                allLength[currentIndex] = lengthThree + places[i3, i4] + places[i4, i]; 
+                                currentIndex++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(allLength.Min());
+        }
+    }
