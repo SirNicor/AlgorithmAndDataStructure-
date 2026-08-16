1387518014b0c25c3090125c19e562718b5baab2
Nicor <nicorbogach@gmail.com>
2026-08-17 00:27:36 +0300

    DuplicateNumber one and two from leetcode

---
+++ LeetCode/ContainsDuplicate/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ContainsDuplicate;
+
+public static class Program
+{
+    public static void Main()
+    {
+        //Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+        int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        Console.WriteLine(HashVariant(nums));
+    }
+
+    private static bool BroodForce(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int i1 = i+1; i1 < nums.Length; i1++)
+            {
+                if (nums[i] == nums[i1])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static bool HashVariant(int[] nums)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        foreach (var t in nums)
+        {
+            if (dict.ContainsKey(t))
+            {
+                return true;
+            }
+            else
+            {
+                dict.Add(t, 1);
+            }
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode/ContainsDuplicateTwo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ContainsDuplicate;
+
+public static class Program
+{
+    public static void Main()
+    {
+        //Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+        int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int k = int.Parse(Console.ReadLine());
+        Console.WriteLine(Hash(nums, k));
+    }
+
+    private static bool Hash(int[] nums, int k)
+    {
+        Dictionary<int, int[]> dict = new Dictionary<int, int[]>();
+        int i = 0;
+        foreach (var t in nums)
+        {
+            if (dict.ContainsKey(t))
+            {
+                if (Math.Abs(i - dict[t][1]) <= k)
+                {
+                    return true;
+                }
+
+                dict[t][1] = i;
+            }
+            else
+            {
+                dict.Add(t, [1, i]);
+            }
+
+            i++;
+        }
+
+        return false;
+    }
+}
