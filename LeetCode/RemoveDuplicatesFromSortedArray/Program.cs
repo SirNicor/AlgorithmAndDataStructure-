4169e1af4b3d6878f8c61ab14a5625c8a4df1cfc
Nicor <nicorbogach@gmail.com>
2026-08-19 00:56:57 +0300

    RemoveDuplicatesFromSortedArray

---
+++ LeetCode/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,32 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();   
+        int k = RemoveDuplicates(nums);
+        foreach (int num in nums)
+        {
+            Console.Write(num + " ");
+        }
+        Console.Write(Environment.NewLine + $"k - {k}");
+    }
+
+    private static int RemoveDuplicates(int[] nums)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        int[] subNums = new int[nums.Length];
+        int i = 0;
+        foreach (var num in nums)
+        {
+            if (dict.ContainsKey(num))
+            {
+                continue;
+            }
+            subNums[i] = num;
+            i++;
+            dict.Add(num, 1);
+        }
+        subNums.CopyTo(nums, 0);
+        return i;
+    }
+}
