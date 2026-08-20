66d460354c4eebfb08a2e1bf5f07ad05b93e3403
Nicor <nicorbogach@gmail.com>
2026-08-20 22:45:16 +0300

    Two sum and valid anagram

---
+++ LeetCode/Two Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int target = Convert.ToInt32(Console.ReadLine());
+        int[] result = TwoSum(nums, target);
+        foreach (int num in result)
+        {
+            Console.WriteLine(num);
+        }
+    }
+
+    private static int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(target - nums[i]))
+            {
+               return [dict[target - nums[i]], i];
+            }
+            if (dict.ContainsKey(nums[i]))
+            {
+                dict[nums[i]] = i;
+            }
+            else
+            {
+                dict.Add(nums[i], i);
+            }
+        }
+
+        return null;
+    }
+}
---
+++ LeetCode/ValidAnagram/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var s = Console.ReadLine();
+        var t = Console.ReadLine();
+        Console.WriteLine(IsAnagram(s, t));
+    }
+
+    private static bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+        Dictionary<char, int> dict =  new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (dict.ContainsKey(s[i]))
+            {
+                dict[s[i]]++;   
+            }
+            else
+            {
+                dict.Add(s[i], 1);
+            }
+            if (dict.ContainsKey(t[i]))
+            {
+                dict[t[i]]--;   
+            }
+            else
+            {
+                dict.Add(t[i], -1);
+            }
+        }
+
+        foreach (var item in dict)
+        {
+            if (item.Value != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
