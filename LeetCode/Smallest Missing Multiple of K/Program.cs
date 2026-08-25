a9e1ab3d37b6a1b91e8a8758e78020769c886a31
Nicor <nicorbogach@gmail.com>
2026-08-25 19:45:36 +0300

    MissingMultiple

---
+++ LeetCode/Smallest Missing Multiple of K/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Programm
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int k = int.Parse(Console.ReadLine());
+        Console.WriteLine(MissingMultiple(nums, k));
+    }
+    
+    private static int MissingMultiple(int[] nums, int k) {
+        HashSet<int> set = new HashSet<int>(nums);
+        int sub = k;
+        while (set.Contains(sub))
+        {
+            sub += k;
+        }
+
+        return sub;
+    }
+}
