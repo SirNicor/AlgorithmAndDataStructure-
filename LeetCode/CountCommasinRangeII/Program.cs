95dc9096b9305727c0dd4dc915f93f790c647d9d
Nicor <nicorbogach@gmail.com>
2026-09-09 20:31:23 +0300

    repair all examples

---
+++ LeetCode/CountCommasinRangeII/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Programm
+{
+    public static void Main()
+    {
+        
+    }
+    
+    public long CountCommas(long n) {
+        long count0 = 1_000_000L - 1_000L;
+        long count1 = 1_000_000_000L - 1_000_000L;
+        long count2 = 1_000_000_000_000L - 1_000_000_000L;
+        long count3 = 1_000_000_000_000_000L - 1_000_000_000_000L;
+        if (n - 999 < 1)
+        {
+            return 0;
+        }
+        if (n < Math.Pow(10, 6))
+        {
+            return n - 999;
+        }
+        if(n < Math.Pow(10, 9))
+        {
+            return count0 + (n - 999 - count0) * 2;
+        }
+        if (n < Math.Pow(10, 12))
+        {
+            return count0 + count1 * 2 + (n - 999 - count0 - count1) * 3;
+        }
+        if (n < Math.Pow(10, 15))
+        {
+            return count0 + count1 * 2 + count2 * 3 + (n - 999 - count0 - count1 - count2) * 4;
+        }
+
+        return count0 + count1 * 2 + count2 * 3 + count3 * 4 + 5;
+    }
+}
---
+++ LeetCode/SmallestStableIndexIe/Program.cs
@@ -0,0 +1,49 @@
+﻿public class Programm
+{
+    public static void Main()
+    {
+        
+    }
+    
+    public static int FirstStableIndex(int[] nums, int k) {
+        int[] arrayWithMaxNumbers = new int[nums.Length], arrayWithMinNumbers = new int[nums.Length];
+        int length = nums.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (i == 0)
+            {
+                arrayWithMaxNumbers[i] = nums[i];
+                arrayWithMinNumbers[length-1-i] = nums[length-1-i];
+                continue;
+            }
+
+            if (nums[i] > arrayWithMaxNumbers[i - 1])
+            {
+                arrayWithMaxNumbers[i] = nums[i];
+            }
+            else
+            {
+                arrayWithMaxNumbers[i] = arrayWithMaxNumbers[i - 1];
+            }
+            
+            if (nums[length-1-i] < arrayWithMinNumbers[length-i])
+            {
+                arrayWithMinNumbers[length-1-i] = nums[length-1-i];
+            }
+            else
+            {
+                arrayWithMinNumbers[length-1-i] = arrayWithMinNumbers[length-i];
+            }
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            if (arrayWithMaxNumbers[i] - arrayWithMinNumbers[i] <= k)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
