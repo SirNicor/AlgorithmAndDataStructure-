dafd19e3f948a6fae60154d7d3ae58ad759ee70f
SirNicor <nicorbogach@gmail.com>
2026-09-09 13:05:49 +0300

    CountCommasinRange full complete and FindFirstAndLastPosition non comp

---
+++ LeetCode/CountCommasinRange/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Programm
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(CountCommas(n));
+    }
+    public static int CountCommas(int n)
+    {
+        int x = n - 999;
+        if (x < 0)
+        {
+            return 0;
+        }
+
+        return x;
+    }
+}
---
+++ LeetCode/CountNegativeNumbersinaSortedMatrix/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Programm
+{
+    public static void Main()
+    {
+        int count = int.Parse(Console.ReadLine());
+        int[][] grid = new int[count][];
+        for (int i = 0; i < count; i++)
+        {
+            grid[i] = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        }
+        Console.WriteLine(CountNegatives(grid));
+    }
+    
+    public static int CountNegatives(int[][] grid)
+    {
+        int count = 0, length = grid[0].Length;
+        foreach (var array in grid)
+        {
+            if (array[length-1] >= 0)
+            {
+                continue;
+            }
+
+            int left = 0, right = length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (array[mid] >= 0)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            count += length - left;
+        }
+        return count;
+    }
+}
